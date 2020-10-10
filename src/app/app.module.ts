46cc776311b2481df028a0c13bd6706262dade8b
hoanganpro2030 <1610132@hcmut.edu.vn>
2020-10-10 23:19:24 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './body/menu/menu.component';
+import { BodyComponent } from './body/body.component';
+import { ContentComponent } from './body/content/content.component';
+import { ProjectFormComponent } from './body/content/project-form/project-form.component';
+import { ProjectListComponent } from './body/content/project-list/project-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+const appRoutes: Routes = [
+  { path: 'project-list', component: ProjectListComponent },
+  { path: 'project-form', component: ProjectFormComponent},
+  { path: '',   redirectTo: 'project-list', pathMatch: 'full' }
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    BodyComponent,
+    ContentComponent,
+    ProjectFormComponent,
+    ProjectListComponent,
+    ErrorComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
